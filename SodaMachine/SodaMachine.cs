f84a8e27b1800750c3c5f468d5bd08fd987e9572
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-17 11:19:39 -0600

    Added a foreach loop in TotalCoinValue Method, to add each coin.Value to a double totalCoinValue and return it (taking from the input List<Coin>)

--- SodaMachine/SodaMachine.cs
+++ SodaMachine/SodaMachine.cs
@@ -141,7 +141,14 @@ namespace SodaMachine
         //Takes in a list of coins to returnt he total value of the coins as a double.
         private double TotalCoinValue(List<Coin> payment)
         {
-           
+            double totalCoinValue = 0;
+
+            foreach (Coin coin in payment)
+            {
+                totalCoinValue += coin.Value;
+            }
+
+            return totalCoinValue;
         }
         //Puts a list of coins into the soda machines register.
         private void DepositCoinsIntoRegister(List<Coin> coins)
