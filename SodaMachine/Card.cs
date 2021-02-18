e049051586790d79999712e6f66c9a0fbc6aaab9
Jacob Werner <jacob.scott.werner@gmail.com>
2021-02-18 09:47:01 -0600

    Created Card Class with protected variable of moneyInAccount, created DepositMoney Method to fill with starting money

---
+++ SodaMachine/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SodaMachine
+{
+    class Card
+    {//Member Variables (Has A)
+        protected double moneyInAccount;
+        //Constructor (Spawner)
+        public Card()
+        {
+            DepositMoney();
+        }
+        //Member Methods (Can Do)
+        //Fills wallet with starting money
+        private void DepositMoney()
+        {
+            moneyInAccount = 4;
+        }
+    }
+}
